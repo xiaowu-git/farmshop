d13bdfe392780a1359981a0ae74e9a39b1157a45
xiaowu-git <2503677254@qq.com>
2019-05-07 00:23:52 +0800

    修改页面

--- src/main/java/com/farm/utils/JedisClient.java
+++ src/main/java/com/farm/utils/JedisClient.java
@@ -1,10 +1,9 @@
 package com.farm.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+
 
 public class JedisClient {
 
